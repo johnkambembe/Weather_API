a32a62ae9af1dccdb4bbf69128e958e0099f6890
johnkambembe <mamitshou04@gmail.com>
2026-09-22 16:34:23 +0200

    fetching data from visualcrossing

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/weather/:city', (req, res) => {
+app.get('/weather/:city', async (req, res) => {
 
     const { city } = req.params;
 
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
     res.json({
-        city : "kinshasa",
-        temparature : 12,
-        condition : 23,
-        humidity : 77
-    })
-})
+            city: data.resolvedAddress,
+            temperature: data.currentConditions.temp,
+            conditions: data.currentConditions.conditions,
+            humidity: data.currentConditions.humidity,
+            windSpeed: data.currentConditions.windspeed,
+            description: data.description
+        });
+
+});
 
 app.listen(PORT, () => {
     console.log(`the server is runnig on ${PORT}`)
