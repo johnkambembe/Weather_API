bb3ce853b951dd2eadb8ad7db0644c2c88b75b0f
johnkambembe <mamitshou04@gmail.com>
2026-09-22 17:22:29 +0200

    update fetching method

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ dotenv.config()
 const app = express()
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 app.get('/weather/:city', async (req, res) => {
 
     const { city } = req.params;
-
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`;
-    const response = await fetch(url);
+     
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`);
     const data = await response.json();
 
     res.json({
