9499fc5fce8d4bc3d83e657da549beb86e96a3bc
johnkambembe <mamitshou04@gmail.com>
2026-09-22 21:45:10 +0200

    controller was added

---
+++ src/controllers/weatherControllers.js
@@ -0,0 +1,22 @@
+
+const weather_api = async (req, res) => {
+
+    const { city } = req.params;
+    
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`);
+    const data = await response.json();
+
+    res.json({
+            city: data.resolvedAddress,
+            temperature: data.currentConditions.temp,
+            conditions: data.currentConditions.conditions,
+            humidity: data.currentConditions.humidity,
+            windSpeed: data.currentConditions.windspeed,
+            description: data.description
+        });
+
+};
+
+export default {
+    weather_api
+};
--- src/routes/weatherRoutes.js
+++ src/routes/weatherRoutes.js
@@ -1,24 +1,9 @@
 
 import express from 'express'
+import controller from '../controllers/weatherControllers.js'
 
 const router = express.Router()
 
-router.get('/:city', async (req, res) => {
-
-    const { city } = req.params;
-    
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`);
-    const data = await response.json();
-
-    res.json({
-            city: data.resolvedAddress,
-            temperature: data.currentConditions.temp,
-            conditions: data.currentConditions.conditions,
-            humidity: data.currentConditions.humidity,
-            windSpeed: data.currentConditions.windspeed,
-            description: data.description
-        });
-
-});
+router.get('/:city', controller.weather_api);
 
 export default router;
